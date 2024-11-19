80abd761c3c86e12bf82a5935fa0c5a888bc1749
K. S. Ernest (iFire) Lee <ernest.lee@chibifire.com>
2024-11-18 17:53:56 -0800

    Pause for now.

--- robust_weight_transfer/core/weight_transfer.gd
+++ robust_weight_transfer/core/weight_transfer.gd
@@ -5,39 +5,8 @@ const program: ELFScript = preload("../robust_weight_transfer.elf")
 func _ready() -> void:
 	set_program(program)
 	set_redirect_stdout(_print)
-	# vmcall("run_tests")
-	var vertices_1 = [
-		Vector3(0, 0, 0),
-		Vector3(1, 0, 0),
-		Vector3(0, 1, 0)
-	]
-	var faces_1 = [
-		[0, 1, 2]
-	]
-	var normals_1 = [
-		Vector3(0, 0, 1),
-		Vector3(0, 0, 1),
-		Vector3(0, 0, 1)
-	]
-	var skin_weights = [
-		[1, 0],
-		[0, 1],
-		[0.5, 0.5]
-	]
-	var vertices_2 = [
-		Vector3(0.1, 0.1, 0),
-		Vector3(2, 2, 2)
-	]
-	var faces_2 = [
-		[0, 1]
-	]
-	var normals_2 = [
-		Vector3(0, 0, 1),
-		Vector3(1, 0, 0)
-	]
-	var result = vmcallv("robust_weight_transfer", vertices_1, faces_1, normals_1, skin_weights, vertices_2, faces_2, normals_2, 0.01, 45.0)
-	print(result)
-	
+	vmcall("run_tests")
+
 
 func _print(line) -> void:
 	print(line)
